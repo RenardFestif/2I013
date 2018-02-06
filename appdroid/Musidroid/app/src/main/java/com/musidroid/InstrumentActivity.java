4c514198e274150ae1d2190cee1f889e7d6cf986
Juba SAADI <saadi_juba@yahoo.fr>
2018-02-06 20:30:12 +0100

    minor changes

--- appdroid/Musidroid/app/src/main/java/com/musidroid/InstrumentActivity.java
+++ appdroid/Musidroid/app/src/main/java/com/musidroid/InstrumentActivity.java
@@ -1,10 +1,15 @@
 package com.musidroid;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ScrollView;
+
+import l2i013.musidroid.util.*;
+
+
 
 
 public class InstrumentActivity extends AppCompatActivity {
@@ -20,8 +25,14 @@ public class InstrumentActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+        /*ScrollView sv = (ScrollView) findViewById(R.id.scrollInstru);
+        CheckBox cb = new CheckBox(getBaseContext());
+        for (InstrumentName instru : InstrumentName.values()){
 
+            cb.setText(instru);
+            sv.addView(cb);
 
+        }*/
 
 
     }
